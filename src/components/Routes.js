9cff43e2227833cc1c5f3dba5a7db98a24e05423
BanhMiDacBiet <banh.mi.dac.biet.ngon@gmail.com>
2020-02-09 19:41:58 -0600

    Removed unnecessary components.

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import DashboardPage from './pages/DashboardPage';
-import SettingsPage from './pages/SettingsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 class Routes extends React.Component {
@@ -10,7 +9,6 @@ class Routes extends React.Component {
       <Switch>
         <Route path='/' exact component={DashboardPage} />
         <Route path='/dashboard' component={DashboardPage} />
-        <Route path='/settings' component={SettingsPage} />
         <Route path='/404' component={NotFoundPage} />
       </Switch>
     );
