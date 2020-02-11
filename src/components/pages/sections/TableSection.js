3561068d5d9f4957ed8d934c9b06621b197ff64d
SimonNguyen <banh.mi.dac.biet.ngon@gmail.com>
2020-02-11 00:58:10 -0600

    Refactored table display and added delete function.

--- src/components/pages/sections/TableSection.js
+++ src/components/pages/sections/TableSection.js
@@ -1,63 +1,110 @@
-import React from 'react';
-import { MDBCard, MDBCardBody, MDBTable, MDBTableBody, MDBTableHead, MDBRow, MDBCol } from 'mdbreact';
+import React, { Component } from 'react';
+import { MDBBadge, MDBCard, MDBCardBody, MDBInput, MDBTable, MDBTableBody, MDBTableHead, MDBRow, MDBCol } from 'mdbreact';
 import data from "./data";
 
-const TableSection = () => {
-    return (
-        <MDBRow>
-            <MDBCol>
-                <MDBCard>
-                    <MDBCardBody>
-                        <MDBTable responsiveMd hover striped>
-                            <MDBTableHead color="blue lighten-4">
-                                <tr>
-                                    {
-                                        data.Headers.map(h => {
-                                            return (
-                                                <th key={h.name}>{h.label}</th>
-                                            )
-                                        })
-                                    }
-                                </tr>
-                            </MDBTableHead>
-                            <MDBTableBody>
-                                {
-                                    data.Recipes.map((r, index) => {
-                                        return (
-                                            <tr key={index}>
-                                                <th>{r.step}</th>
-                                                <th>{r.machine}</th>
-                                                <th>{r.tier}</th>
-                                                <th>{r.overclock.toString()}</th>
-                                                <th>{r.rft}</th>
-                                                <th>{Number(r.time).toPrecision(2)}</th>
-                                                <th>
-                                                    {
-                                                        r.inputs.map((n, index) => {
-                                                            return (
-                                                                <p key={index}>{n.quantity + n.unit + " " + n.name}</p>
-                                                            )
-                                                        })}
-                                                </th>
-                                                <th>
-                                                    {
-                                                        r.outputs.map((o, index) => {
-                                                            return (
-                                                                <p key={index}>{o.quantity + o.unit + " " + o.name}</p>
-                                                            )
-                                                        })}
-                                                </th>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </MDBTableBody>
-                        </MDBTable>
-                    </MDBCardBody>
-                </MDBCard>
-            </MDBCol>
-        </MDBRow>
-    )
+class TableSection extends Component {
+    state = {
+        headers: data.Headers.map(h => {
+            return (h.label)
+        }),
+        recipes: data.Recipes.map(r => {
+            return r
+        })
+    }
+
+    handleDelete = recipeId => {
+        const recipes = this.state.recipes.filter(r => r.step !== recipeId);
+        this.setState({ recipes });
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <MDBRow>
+                    <MDBCol>
+                        <MDBCard>
+                            <MDBCardBody>
+                                <MDBTable responsive hover striped>
+                                    <MDBTableHead color="blue lighten-4">
+                                        {
+                                            (this.state.headers.map(header => (
+                                                <th className="align-middle text-uppercase font-weight-bold">{header}</th>
+                                            )))
+                                        }
+                                    </MDBTableHead>
+                                    <MDBTableBody>
+                                        {
+                                            this.state.recipes.map(recipe => (
+                                                <Recipe
+                                                    key={recipe.step}
+                                                    step={recipe.step}
+                                                    machine={recipe.machine}
+                                                    tier={recipe.tier}
+                                                    overclock={recipe.overclock}
+                                                    rft={recipe.rft}
+                                                    time={recipe.time}
+                                                    inputs={recipe.inputs}
+                                                    outputs={recipe.outputs}
+                                                    onDelete={this.handleDelete}
+                                                />
+                                            ))
+                                        }
+                                    </MDBTableBody>
+                                </MDBTable>
+                            </MDBCardBody>
+                        </MDBCard>
+                    </MDBCol>
+                </MDBRow>
+            </React.Fragment>
+        )
+    }
+}
+
+class Recipe extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <tr>
+                    <th>{this.props.step}</th>
+                    <th>{this.props.machine}</th>
+                    <th>{this.props.tier}</th>
+                    <th>{this.props.overclock.toString()}</th>
+                    <th>{this.props.rft}</th>
+                    <th>{Number(this.props.time).toPrecision(2)}</th>
+                    <th>
+                        {
+                            this.props.inputs.map((n, index) => {
+                                return (
+                                    <div key={index}>
+                                        <b>{n.quantity + n.unit}</b>
+                                        {" " + n.name}
+                                    </div>
+                                )
+                            })}
+                    </th>
+                    <th>
+                        {
+                            this.props.outputs.map((o, index) => {
+                                return (
+                                    <div key={index}>
+                                        <b>{o.quantity + o.unit}</b>
+                                        {" " + o.name}
+                                    </div>
+                                )
+                            })}
+                    </th>
+                    <th>
+                        <MDBBadge
+                            color="danger"
+                            size="sm"
+                            className="float-right"
+                            onClick={() => this.props.onDelete(this.props.step)}
+                        >X</MDBBadge>
+                    </th>
+                </tr>
+            </React.Fragment>
+        )
+    }
 }
 
 export default TableSection;
