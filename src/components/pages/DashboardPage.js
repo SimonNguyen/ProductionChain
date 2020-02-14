6c01ca7d1cbad440d8a5d08c54434ce432e81606
Zel <Zelgoth@live.com>
2020-02-14 17:37:11 -0600

    Added import and call to GetPairs fuction for testing

--- src/components/pages/DashboardPage.js
+++ src/components/pages/DashboardPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import InformationSection from './sections/InformationSection';
 import TableSection from './sections/TableSection';
+import GetPairs from './sections/calculator';
 
 const DashboardPage = () => {
+    GetPairs();
+
     return (
         <React.Fragment>
             <InformationSection />
