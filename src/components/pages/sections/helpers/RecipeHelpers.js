c7696d557b01a9c8007cf7418601d4f0c2192212
Zel <Zelgoth@live.com>
2020-02-15 01:30:08 -0600

    Added String parsing fuction to handle user input/output strings.

--- src/components/pages/sections/helpers/RecipeHelpers.js
+++ src/components/pages/sections/helpers/RecipeHelpers.js
@@ -71,8 +71,8 @@ export function GetNodes(pairs) {
     );
 
     let uniqueNodes = [...new Set(nodes)];
-    uniqueNodes.forEach( (element, index) => 
-        outputNodes.push({ 
+    uniqueNodes.forEach((element, index) =>
+        outputNodes.push({
             name: element,
             color: colors[index % colors.length]
         }));
@@ -81,10 +81,12 @@ export function GetNodes(pairs) {
 }
 
 export function parseItems(raw) {
-    let list = raw.split(',');
+    //Works - needs improvement for readability
+    let list = raw.split(';');
     let items = [];
     for(let index in list){
-        let item = index.split(' ');
+        let item = list[index].split(',');
+        console.log(item);
         items.push(
             {
                 quantity: item[0],
@@ -93,7 +95,7 @@ export function parseItems(raw) {
             }
         )
     };
-    return(items);
+    return (items);
 }
 export function FixLinks(links, nodes) {
     let names = [];
@@ -109,6 +111,6 @@ export function FixLinks(links, nodes) {
             color: link.color
         })
     });
-    
+
     return sankeyLinks;
 }
